7750f3d0fb6f0e442854f649212a913bca18438e
chosungwon <ghj5277@naver.com>
2016-11-12 02:51:52 +0900

    카드나눔

--- portfolio/portfolio.c
+++ portfolio/portfolio.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include"Cards.h"
 int main() {
-	struct card data[4];
+	struct card data[1564];
 	FILE *f;
-	f = fopen("db.txt", "r");
+	f = fopen("card.txt", "r");
 
-	for (int i = 0; i<4; i++)
+	for (int i = 0; i<=1564; i++)
 		fscanf(f, "%s %d %s %s %s %s", data[i].name, &data[i].cost, data[i].job, data[i].grade, data[i].tribe, data[i].category);
 	fclose(f);
 
 
-
-
-	for (int i = 0; i<4; i++)
+	for (int i = 0; i<=1564; i++)
 		printf("%s %d %s %s %s %s\n", data[i].name, data[i].cost, data[i].job, data[i].grade, data[i].tribe, data[i].category);
 
 	return 0;
